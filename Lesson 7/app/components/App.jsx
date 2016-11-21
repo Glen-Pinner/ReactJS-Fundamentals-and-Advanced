9498eb3047f3efb75173cf0ef0e6014e81752e01
Glen Pinner <glen.pinner@gmail.com>
2016-11-21 12:22:15 +0000

    Added code for higher order components (commented out to preserve Profile app).

--- Lesson 7/app/components/App.jsx
+++ Lesson 7/app/components/App.jsx
@@ -4,6 +4,56 @@ import Profile from './Profile.jsx';
 import AddProfile from './AddProfile.jsx';
 import {getProfiles} from '../utils/profileApi';
 
+/*
+ * Higher Order Components
+ *
+ *
+
+let HOCGen = (Component, state) => class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = state;
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            });
+        }, 500);
+    }
+
+    render() {
+        return <Component {...this.props} {...this.state} />
+    }
+};
+
+class Comp1 extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Comp1</p>
+                {this.props.count}
+            </div>
+        );
+    }
+}
+
+class Comp2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Comp2</p>
+                {this.props.count}
+            </div>
+        );
+    }
+}
+
+let WrappedComp1 = HOCGen(Comp1, { count: 0 });
+let WrappedComp2 = HOCGen(Comp2, { count: 100 });
+*/
+
 export default class App extends React.Component {
     constructor(props) {
         super(props);
@@ -47,6 +97,8 @@ export default class App extends React.Component {
             <div>
                 {profiles}
                 <AddProfile addUser={this.addUser} />
+                {/*<WrappedComp1/>*/}
+                {/*<WrappedComp2/>*/}
             </div>
         );
     }
