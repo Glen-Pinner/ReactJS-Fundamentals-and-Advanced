4c73e4b82e56acbc6a3e8ba5971bff75bf5c46f3
Glen Pinner <glen.pinner@gmail.com>
2016-11-22 11:02:58 +0000

    Lesson 9.4: Utilise index routes.

--- Lesson 9/app/index.jsx
+++ Lesson 9/app/index.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, Router, browserHistory} from 'react-router';
+import {Route, IndexRoute, Router, browserHistory} from 'react-router';
 
 import App from './pages/App.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
-
-// class Nav extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={this.props.gotoAbout}>Goto About</button>
-//                 <button onClick={this.props.gotoHome}>Goto Home</button>
-//             </div>
-//         );
-//     }
-// }
 
 /*
 // Exmample of using children rendering
@@ -52,6 +37,7 @@ ReactDOM.render((
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
             <Route path="/home" component={Home}/>
             <Route path="/about" component={About}/>
         </Route>
