73aad51a1095c4ed28e505bdd88fd6ec2ff46a73
Glen Pinner <glen.pinner@gmail.com>
2016-11-22 11:46:40 +0000

    Lesson 10.3: Design your Flux views.

---
+++ Lesson 10/app/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        };
+
+        this.increment = this.increment.bind(this);
+    }
+
+    increment(e) {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.increment}>Increment count</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
