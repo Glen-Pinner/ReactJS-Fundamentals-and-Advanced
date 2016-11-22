77a5dd986273f5088d61ef7fabac680442cb011e
Glen Pinner <glen.pinner@gmail.com>
2016-11-22 12:04:01 +0000

    Lesson 10.4: Define action constants.

---
+++ Lesson 10/app/actions/app-actions.js
@@ -0,0 +1,7 @@
+/**
+ * Created by glenp on 22/11/2016.
+ */
+
+import constants from '../constants/app-constants';
+
+console.log(constants);
---
+++ Lesson 10/app/constants/app-constants.js
@@ -0,0 +1,9 @@
+/**
+ * Created by glenp on 22/11/2016.
+ */
+
+const constants = {
+    INCREMENT: 'INCREMENT'
+};
+
+export default constants;
